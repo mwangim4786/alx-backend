7158dae32eeade112ecf8f91934a5c02838a5f6c
Martin Kihungi <mwangim4786@gmail.com>
2024-09-03 01:09:23 +0000

    0-simple_helper_function.py 2

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,5 +9,6 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ return in a list for pagination parameters. """
     start = (page - 1) * page_size
     end = start + page_size
+
     return (start, end)
 
