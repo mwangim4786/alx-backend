86994e9277d2134544abc5fa7d8f6cc769c12bc8
Martin Kihungi <mwangim4786@gmail.com>
2024-09-03 00:43:05 +0000

    2-hypermedia_pagination.py

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+""" Implement a method named get_page that takes two integer arguments
+    page with default value 1 and page_size with default value 10.
+"""
+
+import csv
+from math import ceil
+from typing import Tuple
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Output page of dataset. """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+    
+        tuple = index_range(page, page_size)
+        start = tuple[0]
+        end = tuple[1]
+        
+        try:
+            return self.dataset()[start:end]
+        except IndexError:
+            return []
+
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """ Return dictionary of pagination data. """
+        page_data = self.get_page(page, page_size)
+        total_data = len(self.dataset())
+        total_pages = ceil(total_data / page_size)
+
+        return {
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next_page': page + 1 if page < total_pages else None,
+            'prev_page': page - 1 if page != 1 else None,
+            'total_pages': total_pages
+        }
+
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ return in a list for pagination parameters. """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
