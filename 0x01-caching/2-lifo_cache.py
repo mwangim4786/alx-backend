55d7c968b9fe7e89271fd44f0eb92630c91c4559
Martin Kihungi <mwangim4786@gmail.com>
2024-09-05 01:59:28 +0300

    2-lifo_cache.py

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Create a class LIFOCache that inherits from BaseCaching and is a caching system: """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ Defines class LIFOCache """
+
+    def __init__(self):
+        """ Initializes LIFOCache """
+        self.stack = []
+        super().__init__()
+
+    def put(self, key, item):
+        """ Assign item to dictionary """
+        if key and item:
+            if self.cache_data.get(key):
+                self.stack.remove(key)
+            while len(self.stack) >= self.MAX_ITEMS:
+                delete = self.stack.pop()
+                self.cache_data.pop(delete)
+                print('DISCARD: {}'.format(delete))
+            self.stack.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key """
+        return self.cache_data.get(key)
