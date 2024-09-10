8f6aff43746d3f19d389cc45cf8ff6084fa9ac59
Martin Kihungi <mwangim4786@gmail.com>
2024-09-10 11:35:35 +0000

    0-app.py 1

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,17 +1,16 @@
-from flask import Flask, render_template
+#!/usr/bin/python3
+"""Script that starts a Flask web application
+"""
 
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    """Returns string Hello world when route is queried
+    """
     return render_template("0-index.html")
-
-
-
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
