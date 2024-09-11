fe37c81d75aaf95cf9ba316bca405562a189a0f3
Martin Kihungi <mwangim4786@gmail.com>
2024-09-11 00:21:50 +0000

    1-app.py

---
+++ 0x02-i18n/routes/route1.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+app_routes = Blueprint("app_routes", __name__, template_folder="templates")
+
+@app_routes.route("/")
+def index():
+    return render_template("1-index.html")
